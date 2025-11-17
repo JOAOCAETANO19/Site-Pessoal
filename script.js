dd5f55ebb90596816be4c949522b99cb9c31fe3b
JOAOCAETANO19 <joao.cleberson.caetano@escola.pr.gov.br>
2025-11-17 12:08:02 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+// EFEITO DE DIGITAÇÃO
 const texto = "Bem-vindo ao meu site!";
 let index = 0;
 
 function digitar() {
-    const elemento = document.getElementById("texto-digitado");
+  const h1 = document.getElementById("digitar");
+  h1.textContent = texto.substring(0, index);
+  index++;
 
-    if (index < texto.length) {
-        elemento.textContent += texto.charAt(index);
-        index++;
-        setTimeout(digitar, 80);
-    }
+  if (index <= texto.length) {
+    setTimeout(digitar, 120);
+  }
 }
 
-window.onload = digitar;
+digitar();
+
+// ANIMAÇÃO AO ROLAR
+function revelarAoRolar() {
+  const elementos = document.querySelectorAll(".foto, .item");
+
+  elementos.forEach((el, i) => {
+    const pos = el.getBoundingClientRect().top;
+    const altura = window.innerHeight - 100;
+
+    if (pos < altura) {
+      setTimeout(() => {
+        el.classList.add("ativo");
+      }, i * 150);
+    }
+  });
+}
+
+window.addEventListener("scroll", revelarAoRolar);
+window.addEventListener("load", revelarAoRolar);
