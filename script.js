8e225062d1c9d5349e75d51a1d18344bd19fe2c9
JOAOCAETANO19 <joao.cleberson.caetano@escola.pr.gov.br>
2025-11-17 15:24:38 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* === ANIMAÇÃO DE MÁQUINA DE ESCREVER === */
+/* === MÁQUINA DE ESCREVER DO TÍTULO === */
 const text = "Bem-vindo ao meu site!";
 let index = 0;
 
@@ -11,6 +11,20 @@ function typeWriter() {
 }
 
 typeWriter();
+
+/* === MÁQUINA DE ESCREVER DO NOME === */
+const text2 = "por João Caetano";
+let index2 = 0;
+
+function typeWriter2() {
+  if (index2 < text2.length) {
+    document.getElementById("subTypewriter").textContent += text2.charAt(index2);
+    index2++;
+    setTimeout(typeWriter2, 70);
+  }
+}
+
+setTimeout(typeWriter2, 1500);
 
 /* === ANIMAÇÃO DE APARECER SUAVEMENTE === */
 const items = document.querySelectorAll('.fade-item');
