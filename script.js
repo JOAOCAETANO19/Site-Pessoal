9ba36209690ba30102e23bda6917f7e360e288ce
JOAOCAETANO19 <joao.cleberson.caetano@escola.pr.gov.br>
2025-11-17 15:07:08 -0300

    .

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-/* === ANIMAÇÃO DE MÁQUINA DE ESCREVER === */
 const text = "Bem-vindo ao meu site!";
 let index = 0;
 
 function typeWriter() {
-  if (index < text.length) {
-    document.getElementById("typewriter").textContent += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, 80);
-  }
+    document.querySelector(".typing").textContent = text.slice(0, index);
+
+    if (index < text.length) {
+        index++;
+        setTimeout(typeWriter, 90);
+    }
 }
 
 typeWriter();
-
-/* === ANIMAÇÃO DE APARECER SUAVEMENTE === */
-const items = document.querySelectorAll('.fade-item');
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('ativo');
-    }
-  });
-}, { threshold: 0.1 });
-
-items.forEach(item => {
-  observer.observe(item);
-});
