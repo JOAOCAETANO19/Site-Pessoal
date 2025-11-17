3e2c5066339ede75b42f1d85d7e9498fd0f991a3
JOAOCAETANO19 <joao.cleberson.caetano@escola.pr.gov.br>
2025-11-17 11:54:52 -0300

    .

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Texto que será digitado
 const texto = "Bem-vindo ao meu site!";
 let index = 0;
-const velocidade = 80;
 
-// Função de digitação
 function digitar() {
     const elemento = document.getElementById("texto-digitado");
 
     if (index < texto.length) {
         elemento.textContent += texto.charAt(index);
         index++;
-        setTimeout(digitar, velocidade);
+        setTimeout(digitar, 80);
     }
 }
 
